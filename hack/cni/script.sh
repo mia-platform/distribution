89f9766e8a4af84df46050b7a621e84d607d400d
Jacopo Andrea Giola <jacopo@giola.org>
2026-01-07 16:16:53 +0100

    feat: update cilium version in cni module

--- hack/cni/script.sh
+++ hack/cni/script.sh
@@ -3,7 +3,7 @@
 set -o nounset
 set -o errexit
 
-CILIUM_VERSION="${1:-1.18.4}"
+CILIUM_VERSION="${1:-1.18.5}"
 FOLDER_PATH=$(dirname -- "${0}")
 
 helm repo add cilium https://helm.cilium.io/
@@ -11,27 +11,27 @@ helm repo add cilium https://helm.cilium.io/
 helm repo update cilium
 
 helm template cilium cilium/cilium \
-	--kube-version "v1.33.0-0" \
+	--kube-version "v1.34.0-0" \
 	--version "${CILIUM_VERSION}" \
 	--namespace kube-system \
 	--values "${FOLDER_PATH}/values.yaml" > "${FOLDER_PATH}/kind-${CILIUM_VERSION}.yaml"
 
 helm template cilium cilium/cilium \
-	--kube-version "v1.33.0-0" \
+	--kube-version "v1.34.0-0" \
 	--version "${CILIUM_VERSION}" \
 	--namespace kube-system \
 	--values "${FOLDER_PATH}/values.yaml" \
 	--values "${FOLDER_PATH}/eks-values.yaml" > "${FOLDER_PATH}/eks-${CILIUM_VERSION}.yaml"
 
 helm template cilium cilium/cilium \
-	--kube-version "v1.33.0-0" \
+	--kube-version "v1.34.0-0" \
 	--version "${CILIUM_VERSION}" \
 	--namespace kube-system \
 	--values "${FOLDER_PATH}/values.yaml" \
 	--values "${FOLDER_PATH}/gke-values.yaml" > "${FOLDER_PATH}/gke-${CILIUM_VERSION}.yaml"
 
 helm template cilium cilium/cilium \
-	--kube-version "v1.33.0-0" \
+	--kube-version "v1.34.0-0" \
 	--version "${CILIUM_VERSION}" \
 	--namespace kube-system \
 	--values "${FOLDER_PATH}/values.yaml" \
--- modules/cni/cilium/CHANGELOG.md
+++ modules/cni/cilium/CHANGELOG.md
@@ -7,27 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
-## v1.33.3
-
 ### Changed
 
-- update cilium to v1.18.4
+- update cilium to v1.18.5
 
-## v1.33.2
+### Added
 
-### Changed
-
-- update cilium to v1.18.3
-
-## v1.33.1
-
-### Changed
-
-- update cilium to v1.18.2
-- update hubble UI to v0.13.3
-
-## v1.33.0
-
-### Changed
-
-- update cilium to v1.18.1
+- add external envoy daemonset and enable l7 loadbalancer for grpc
